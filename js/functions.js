ed9f918548bebdbf93484e597a8118b3640058a8
Давлетчин Сергей <serj.davletchin@gmail.com>
2023-10-28 15:31:07 +0500

    Вторая функция

--- js/functions.js
+++ js/functions.js
@@ -6,3 +6,22 @@ const checkStringLength = (string, maxLength) => string.length <= maxLength;
 checkStringLength('проверяемая строка', 20); // возвращает true
 checkStringLength('проверяемая строка', 18); // возвращает true
 checkStringLength('проверяемая строка', 10); // возвращает false
+
+// 2.  Функция на проверку палиндрома
+
+const checkPalindrome = (string) => {
+  const stringNormalize = string.replaceAll (' ','').toUpperCase();
+  let stringCheck = '';
+  for (let i = stringNormalize.length - 1; i > stringNormalize.indexOf([0]); i --) {
+    stringCheck += stringNormalize.at(i);
+  }
+  if (stringCheck !== stringNormalize) {
+    return false;
+  }
+  return true;
+};
+
+checkPalindrome('топот'); // Возвращает true
+checkPalindrome('ДоВоД '); // Возвращает true
+checkPalindrome('Кекс'); // Возвращает false
+checkPalindrome('Лёша на полке клопа нашёл '); // Возвращает true
