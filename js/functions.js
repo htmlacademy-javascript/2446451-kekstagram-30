ec85584412376429b31d149b04bb8b339bd59a2b
Давлетчин Сергей <serj.davletchin@gmail.com>
2023-10-28 21:08:31 +0500

    Добавил третью функцию

--- js/functions.js
+++ js/functions.js
@@ -1,3 +1,5 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-empty */
 // 1. Функция на проверку длины строки
 
 
@@ -25,3 +27,25 @@ checkPalindrome('топот'); // Возвращает true
 checkPalindrome('ДоВоД '); // Возвращает true
 checkPalindrome('Кекс'); // Возвращает false
 checkPalindrome('Лёша на полке клопа нашёл '); // Возвращает true
+
+// 3. Функция извлечения целых положительных чисел из строки
+
+const getIntNubmer = (string) => {
+  const stringCheck = string.toString();
+  let result = '';
+  for (let i = 0; i < stringCheck.length; i ++) {
+    if (!Number.isNaN(parseInt(stringCheck[i], 10))) {
+      result += stringCheck[i];
+    }
+  }
+  return parseInt (result, 10);
+};
+
+getIntNubmer('2023 год'); // 2023
+getIntNubmer('ECMAScript 2022'); // 2022
+getIntNubmer('1 кефир, 0.5 батона'); //  105
+getIntNubmer('агент 007'); // 7
+getIntNubmer('а я томат'); // NaN
+getIntNubmer(2023); // 2023
+getIntNubmer(-1); // 1
+getIntNubmer(1.5); // 15
